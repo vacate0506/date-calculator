0c6611d135e014aa4bc96d8cd0c113a46de020de
vacate <vacate0506@gmail.com>
2026-09-14 20:03:36 +0800

    数据更新

--- src/data.ts
+++ src/data.ts
@@ -25,12 +25,14 @@ export const initialData: Record<Platform, PlatformStats> = {
       { x: '2026-07-29', y: 7 },
       { x: '2026-08-09', y: 10 },
       { x: '2026-09-01', y: 11 },
+      { x: '2026-09-14', y: 9 },
     ],
     likes_collections: [
       { x: '2026-07-14', y: 667 },
       { x: '2026-07-29', y: 940 },
       { x: '2026-08-09', y: 1132 },
       { x: '2026-09-01', y: 1372 },
+      { x: '2026-09-14', y: 1866 },
     ],
     color_f: 'rgb(0, 174, 236)',  // 粉丝颜色（B站蓝）
     color_lc: 'rgb(0, 139, 139)',  // 获赞颜色（青色）
@@ -40,10 +42,12 @@ export const initialData: Record<Platform, PlatformStats> = {
     followers: [
       { x: '2026-08-09', y: 4 },
       { x: '2026-09-01', y: 7 },
+      { x: '2026-09-14', y: 8 },
     ],
     likes_collections: [
       { x: '2026-08-09', y: 47 },
       { x: '2026-09-01', y: 73 },
+      { x: '2026-09-14', y: 105 },
     ],
     color_f: 'rgb(255, 69, 0)',   // 粉丝颜色（小红书红）
     color_lc: 'rgb(255, 140, 0)', // 获赞颜色（深橙色）
